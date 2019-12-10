79267ec9cc5ab2c12067bfb82b883711be2a77cb
Martin Peka <martin.peka@argo22.com>
2019-12-10 12:22:43 +0100

    Increase gateway timeout

--- src/checkpoint_docker_gateway/index.php
+++ src/checkpoint_docker_gateway/index.php
@@ -13,7 +13,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+curl_setopt($ch, CURLOPT_TIMEOUT, 300);
 
 switch ($_SERVER['REQUEST_METHOD']) {
 case "GET":
